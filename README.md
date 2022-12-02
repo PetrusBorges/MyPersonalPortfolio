63602901028112bf4780bf20f02f66a4d6282ee6
Petrus Borges Machado <contatopetrusborges@gmail.com>
2022-12-02 12:18:29 -0300

    Inclusão Portfólio

---
+++ README.md
@@ -0,0 +1,28 @@
+<div align="center">
+  First iteration of my personal website built with <a href="https://reactjs.org/docs/getting-started.html" target="_blank">ReactJS</a> + <a href="https://styled-         components.com" target="_blank">Stytled Components</a> hosted with <a href="vercel.com/" target="_blank">Vercel</a>.
+</div></br>
+
+<p align="center">
+  <a href="https://petrusborges.vercel.app" target="_blank">
+    My Portfolio
+  </a>
+</p>
+
+##
+
+![image](https://user-images.githubusercontent.com/105453766/201519113-0d4b4874-7f6c-475c-b989-f7b083443fde.png)
+
+# 💻 Personal Website
+
+Hello! Thanks for checking out the repo for my latest portfolio website.
+
+* What I used to build
+
+  * <a href="https://reactjs.org/docs/getting-started.html" target="_blank">ReactJS</a>
+  * <a href="https://styled-components.com" target="_blank">Stytled Components</a>
+
+# ✒️ Authors
+
+[Petrus Borges - GitHub](https://github.com/PetrusBorges)
+
+[Petrus Borges - Linkedin](https://www.linkedin.com/in/petrusborgesmachado/)
---
+++ src/components/App/index.js
@@ -0,0 +1,52 @@
+//styled-comments
+import Theme from "../../assets/styles/themes"
+import GlobalStyled from "../../assets/styles/global"
+import { Container } from "./styles"
+import { ThemeProvider } from "styled-components"
+
+//hooks
+import { useEffect, useMemo, useState } from "react"
+
+//components
+import Header from "../Header"
+import Content from "../Content"
+import About from "../About"
+import Projects from "../Projects"
+import OtherProjects from "../OtherProjects"
+import GetInTouch from "../GetInTouch"
+import ButtonTheme from "../ButtonTheme"
+
+export default function App() {
+
+  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem("Theme")) || 'Light')
+
+  const currentTheme = useMemo(() => {
+    return Theme[theme] || Theme.Dark
+  }, [theme])
+
+  function handleToggleTheme() {
+    setTheme((prevState) => prevState === "Dark" ? "Light" : "Dark")
+  }
+
+  useEffect(() => {
+    localStorage.setItem("Theme", JSON.stringify(theme))
+  }, [theme])
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <GlobalStyled />
+      <ButtonTheme
+        onClick={handleToggleTheme}
+        selectedTheme={theme}
+      />
+      <Container>
+        <Header />
+        <Content />
+        <About />
+        <Projects />
+        <OtherProjects />
+        <GetInTouch />
+      </Container>
+    </ThemeProvider>
+  )
+}
